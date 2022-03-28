5f8ab0ada8a8c433010ef04aa10160e3357bad41
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-03-28 08:31:50 -0300

    Added subnet files

---
+++ terraform/subnets.tf
@@ -0,0 +1,17 @@
+resource "google_compute_subnetwork" "private" {
+  name                     = "private"
+  ip_cidr_range            = "10.0.0.0/18" # 16 ips
+  region                   = "us-east1"
+  network                  = google_compute_network.main.id
+  private_ip_google_access = true
+
+  secondary_ip_range {
+    range_name    = "k8s-pod-range"
+    ip_cidr_range = "10.48.0.0/14"
+  }
+
+  secondary_ip_range {
+    range_name    = "k8s-service-range"
+    ip_cidr_range = "10.52.0.0/20"
+  }
+}
